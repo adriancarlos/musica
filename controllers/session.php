d9f7f87a8dd685fe4ee70425e3e86e8acdad0514
adriancarlos <47173768+adriancarlos@users.noreply.github.com>
2019-02-17 22:10:42 +0100

    Update session.php

--- controllers/session.php
+++ controllers/session.php
@@ -7,7 +7,7 @@
    
    $user_check = $_SESSION['login_user'];
    
-   $ses_sql = mysqli_query($conn,"select FirstName from customer where Email = '$user_check' ;");
+   $ses_sql = mysqli_query($conn,"select FirstName from Customer where Email = '$user_check' ;");
    
    $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
    
