84113af970a0a30ac71796c04ee93324eab8d81f
adriancarlos <47173768+adriancarlos@users.noreply.github.com>
2019-02-18 09:11:55 +0100

    Update config.php

--- db/config.php
+++ db/config.php
@@ -2,7 +2,7 @@
    define('DB_SERVER', '10.128.15.103');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'rootroot');
-   define('DB_DATABASE', 'musica');
+   define('DB_DATABASE', 'appmusic');
    $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    
    if (!$conn) {
